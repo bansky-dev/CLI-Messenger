5c01467e56c795cb7c87503def25a1b9ad3b3f89
Kociarz <dbednorz@tuta.io>
2024-09-21 13:43:53 +0200

    Add project files.

---
+++ Messeaging-app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+
+namespace Messeaging_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+              
+
+        }
+
+        private void sendMesseage()
+        {
+            Console.WriteLine("sending messeage!....");
+            Console.WriteLine("messeage sent");
+        }
+
+    }
+}
